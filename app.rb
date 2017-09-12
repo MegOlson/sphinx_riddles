4cd8b36e537c5c97c526f67142979b82736ddfe5
Luciano Oliveira and Megan Olson <luciano@>
2017-09-12 10:32:27 -0700

    fix result page

--- app.rb
+++ app.rb
@@ -14,5 +14,10 @@ post('/output') do
   riddle3 = params["riddle3"]
   riddle = Riddles.new(riddle1, riddle2, riddle3)
   @test = riddle.result
+  if @test == true
+    @fate = "You made it to the ancient city!"
+  else
+    @fate = "You answered one or more riddles incorecctly, try again to reach the Ancient City."
+  end
   erb(:output)
 end
